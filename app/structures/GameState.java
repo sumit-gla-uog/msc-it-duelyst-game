cc1a45570ca156e50e9ca9440b8d6f4bd19c8855
YimingWanggla <3088495W@student.gla.ac.uk>
2026-02-28 10:01:01 +0000

    SC-104: Deal Starting Hand
    Implemented SC-104. Added deck and hand lists to the Player class. Updated Initialize.java to deal 3 starting cards directly from the config without shuffling, as per game rules.

--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,5 +1,7 @@
 package structures;
 
+import structures.basic.Player;
+
 /**
  * This class can be used to hold information about the on-going game.
  * Its created with the GameActor.
@@ -13,5 +15,11 @@ public class GameState {
 	public boolean gameInitalised = false;
 	
 	public boolean something = false;
-	
+
+	public Player humanPlayer;
+	public Player aiPlayer;
+	public GameState() {
+		this.humanPlayer = new Player();
+		this.aiPlayer = new Player();
+	}
 }
